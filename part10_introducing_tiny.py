99a298702c2522c15293eb73777e887e9e9d37b6
Eli Bendersky <eliben@gmail.com>
2025-11-11 06:54:48 -0800

    Starting skeleton for part 10

---
+++ part10_introducing_tiny.py
@@ -0,0 +1,58 @@
+from typing import TextIO
+import sys
+
+
+class Compiler:
+    def __init__(self, src: str, output: TextIO = sys.stdout):
+        self.src = src
+        self.pos = 0
+        self.look = ""
+        self.output = output
+        self.get_char()
+
+    def get_char(self):
+        if self.pos < len(self.src):
+            self.look = self.src[self.pos]
+            self.pos += 1
+        else:
+            self.look = ""  # End of input
+
+    def abort(self, msg: str):
+        raise Exception(f"Error: {msg}")
+
+    def expected(self, s: str):
+        self.abort(f"{s} expected")
+
+    def match(self, x: str):
+        if self.look == x:
+            self.get_char()
+        else:
+            self.expected(f"'{x}'")
+
+    def get_name(self) -> str:
+        if not self.look.isalpha():
+            self.expected("Name")
+        name = self.look.upper()
+        self.get_char()
+        return name
+
+    def emit(self, s: str):
+        self.output.write("    " + s)
+
+    def emit_ln(self, s: str):
+        self.emit(s + "\n")
+
+    def prolog(self, name: str):
+        self.emit_ln(f"; Module {name}")
+        self.emit_ln("(module")
+
+    def epilog(self):
+        self.emit_ln(")")
+
+    def prog(self):
+        self.match("p")
+        # TODO: need header, or not??
+        self.prolog()
+        self.match(".")
+        self.epilog()
+
